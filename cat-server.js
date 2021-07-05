02694ac7defe18d3db5d00caf7163439a9648c8c
Alejandro Monasterios <alejandro.monasterios@gmail.com>
2021-07-05 16:01:06 -0400

    add cat & cat server examples

---
+++ cat-server.js
@@ -0,0 +1,26 @@
+const concat = require('concat-stream')
+const through = require('through2')
+const http = require('http')
+const qs = require('querystring')
+
+const server = http.createServer((req, res) => {
+	req
+	  .pipe(counter())
+		.pipe(concat({encoding: 'string'}, onbody)) 
+	
+	function counter() {
+		let size = 0
+		return through(function(buf, enc, next) {
+			size += buf.length
+			if (size > 20) next(null, null)
+			else next(null, buf)
+		})
+	}
+	function onbody(body) {
+		const params = qs.parse(body)
+		console.log(params)
+		res.end('ok\n')
+	} 
+})
+
+server.listen(5000)
---
+++ cat.js
@@ -0,0 +1,5 @@
+const concat = require('concat-stream')
+
+process.stdin.pipe(concat(function(body){
+	console.log(body.length)
+}))
